55e56bfc019104869bd904afadcc2ea6fd87d303
BIGUENET DENIS <dbiguene@localhost.localdomain>
2020-02-06 09:11:23 +0100

    Add loadGltfFile method.

--- apps/gltf-viewer/ViewerApplication.cpp
+++ apps/gltf-viewer/ViewerApplication.cpp
@@ -11,7 +11,8 @@
 #include "utils/cameras.hpp"
 
 #include <stb_image_write.h>
-#include <tiny_gltf.h>
+
+using namespace std;
 
 void keyCallback(
     GLFWwindow *window, int key, int scancode, int action, int mods)
@@ -55,7 +56,7 @@ int ViewerApplication::run()
   }
 
   tinygltf::Model model;
-  // TODO Loading the glTF file
+  loadGltfFile(model);
 
   // TODO Creation of Buffer Objects
 
@@ -181,4 +182,27 @@ ViewerApplication::ViewerApplication(const fs::path &appPath, uint32_t width,
   glfwSetKeyCallback(m_GLFWHandle.window(), keyCallback);
 
   printGLVersion();
+}
+
+bool ViewerApplication::loadGltfFile(tinygltf::Model & model) {
+	tinygltf::TinyGLTF loader;
+	std::string err;
+	std::string warn;
+
+	bool ret = loader.LoadASCIIFromFile(&model, &err, &warn, m_gltfFilePath.string());
+	//bool ret = loader.LoadBinaryFromFile(&model, &err, &warn, argv[1]); // for binary glTF(.glb)
+
+	if (!warn.empty()) {
+	  printf("Warn: %s\n", warn.c_str());
+	}
+
+	if (!err.empty()) {
+	  printf("Err: %s\n", err.c_str());
+	}
+
+	if (!ret) {
+	  printf("Failed to parse glTF\n");
+	  return -1;
+	}
+
 }
